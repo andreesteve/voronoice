2ddbe7f1cd306a669e53bc5ea5b2c503fc68cc21
Andre Esteve <andreesteve@gmail.com>
2022-02-18 17:47:19 -0800

    Add 'quick' to bench group name

--- benches/quick.rs
+++ benches/quick.rs
@@ -4,9 +4,11 @@ mod bench_base;
 use bench_base::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("100 random sites", |b| create_benchmark_fn(b, 100));
-    c.bench_function("1,000 random sites", |b| create_benchmark_fn(b, 1_000));
-    c.bench_function("10,000 random sites", |b| create_benchmark_fn(b, 10_000));
+    let mut group = c.benchmark_group("quick");
+    group.bench_function("100 random sites", |b| create_benchmark_fn(b, 100));
+    group.bench_function("1,000 random sites", |b| create_benchmark_fn(b, 1_000));
+    group.bench_function("10,000 random sites", |b| create_benchmark_fn(b, 10_000));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
